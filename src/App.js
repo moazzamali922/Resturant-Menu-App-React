a1e91bb401b546a697f294a2405050287ca90dc6
moazzamali922 <121150980+moazzamali922@users.noreply.github.com>
2023-05-18 00:04:59 +0500

    Resturant App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resturant from './Component/Basic/Resturant';
+
+
+export const App = () => {
+  return (
+    <div><Resturant/></div>
+
+  )
+}
+
+export default App;
---
+++ src/Component/Basic/menuApi.js
@@ -0,0 +1,88 @@
+const Menu = [
+    {
+      id: 1,
+      image: "Images/Bread.png",
+      name: "Bread",
+      category: "breakfast",
+      price: "12pkr",
+      description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "Images/potatochips.png",
+      name: "Potato Chips",
+      category: "evening",
+      price: "20pkr",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "Images/yougart.png",
+      name: "Yougart",
+      category: "breakfast",
+      price: "10pkr",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "Images/Biryani.png",
+      name: "Biryani",
+      category: "lunch",
+      price: "50pkr",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "Images/cake.png",
+      name: "Cake",
+      category: "evening",
+      price: "80pkr",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "Images/BBQ.png",
+      name: "BBQ",
+      category: "dinner",
+      price: "180pkr",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "Images/pulao.png",
+      name: "Pulao",
+      category: "dinner",
+      price: "60pkr",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "Images/Chinese.png",
+      name: "Chinese Rice",
+      category: "lunch",
+      price: "60pkr",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "Images/Biscuit.png",
+      name: "Biscuit",
+      category: "evening",
+      price: "10pkr",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+  ];
+  
+  export default Menu;
+  
